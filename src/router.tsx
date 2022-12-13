6e2c88890d2698c7e1000b5910e168ec5e58d794
JLarky <jlarky@gmail.com>
2022-12-12 23:14:09 -0700

    is that hot or what?
    - vite SPA (locator js, HMR and stuff)
    - gets data from Astro

--- src/router.tsx
+++ src/router.tsx
@@ -2,12 +2,36 @@ import { createReactRouter } from '@tanstack/react-router'
 import { routeConfig } from './routes.generated/routeConfig'
 import { routeConfigClient } from './routes.generated/routeConfig.client'
 
-export const createRouter = () =>
-  createReactRouter({
+export const createRouter = () => {
+  const router = createReactRouter({
     routeConfig:
       typeof document !== 'undefined' ? routeConfigClient : routeConfig,
     useServerData: true,
   })
+  if (process.env.VITE) {
+    // loadMatchData from packages/router-core/src/router.ts
+    router.loadMatchData = async (routeMatch) => {
+      const next = router.buildNext({
+        to: '.',
+        search: (d: any) => ({
+          ...(d ?? {}),
+          __data: {
+            matchId: routeMatch.matchId,
+          },
+        }),
+      })
+      const nextAstro = new URL(next.href, 'http://127.0.0.1:3000/api')
+      const res = await fetch(nextAstro.href, {
+        method: 'GET',
+      })
+      if (res.ok) {
+        return res.json()
+      }
+      throw new Error('Failed to fetch match data')
+    }
+  }
+  return router
+}
 
 declare module '@tanstack/react-router' {
   interface RegisterRouter {
